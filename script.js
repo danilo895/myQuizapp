8cf794ef872a8d46ecf9c09c7e64c5cc0863b994
Daniel Loeffler <loefflerdaniel@web.de>
2024-10-28 10:38:37 +0100

    updated

--- script.js
+++ script.js
@@ -40,7 +40,10 @@ let questions = [{
 }
 ];
 
+let rightQuestions = 0;
 let currentQuestion = 0;
+let audio_success = new Audio('audio/right.ogg');
+let audio_fail = new Audio('audio/wrong.mp3');
 
 function init(){
     document.getElementById('all-questions').innerHTML = questions.length;
@@ -48,18 +51,36 @@ showQuestion()
 }
 
 function showQuestion(){
-    if(currentQuestion >= questions.length){
-    document.getElementById('endScreen').style = '';
-    document.getElementById('questionBody').style = 'display: none';
-    }else{
+    if(currentQuestion >= questions.length){ // SHOW ENDSCREEN
+    showEndScreen()
+    }else{ // SHOW NEXT QUESTION
+    showAnotherquestion();    
+    }
+}
+
+function showAnotherquestion(){
+    let percent = (currentQuestion + 1) / questions.length;
+    percent = percent * 100;
+    document.getElementById('progress-bar').innerHTML = `${percent} %`;
+    document.getElementById('progress-bar').style = `width: ${percent}%;`;
+
+
 let question = questions[currentQuestion];
+console.log('Fortschritt', percent);
 document.getElementById("question-number").innerHTML = currentQuestion +1;
 document.getElementById('questiontext').innerHTML = question['question'];
 document.getElementById('answer_1').innerHTML = question['answer_1'];
 document.getElementById('answer_2').innerHTML = question['answer_2'];
 document.getElementById('answer_3').innerHTML = question['answer_3'];
 document.getElementById('answer_4').innerHTML = question['answer_4'];
-    }
+}
+
+function showEndScreen(){
+    document.getElementById('endScreen').style = '';
+    document.getElementById('questionBody').style = 'display: none';
+    document.getElementById('amount-of-questions').innerHTML = questions.length;
+    document.getElementById('amount-of-right-questions').innerHTML = rightQuestions;
+    document.getElementById('background-to-change').src = './assets/mytrophy.png'
 }
 
 function answer(selection){ //selection definiere ich hier neu und kommt aus dem HTML onclick="answer('answer_1')"
@@ -67,14 +88,17 @@ function answer(selection){ //selection definiere ich hier neu und kommt aus dem
     let selectedQuestionNumber = selection.slice(-1) // ich nehme aus meinem html "answer_x" den letzten Buchstaben 1 bis 4
     let idOfRightAnswer = `answer_${question['right_answer']}`
 
-    if(selectedQuestionNumber == question['right_answer']){
+    if(selectedQuestionNumber == question['right_answer']){ // richtige Frage beantwortet
         console.log('richtig!')
         document.getElementById(selection).parentNode.classList.add('bg-success')
+        audio_success.play();
+        rightQuestions++;
     }
     else{
         console.log('falsch!')
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        audio_fail.play();
     }
     document.getElementById('next-button').disabled = false;
     
@@ -96,4 +120,13 @@ function resetAnswerButtons(){
     document.getElementById('answer_3').parentNode.classList.remove('bg-success');
     document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
     document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
+
+function restartGame(){
+    document.getElementById('background-to-change').src = "./assets/background-cards.jpg"
+    document.getElementById('endScreen').style = 'display: none';
+    document.getElementById('questionBody').style = '';
+    rightQuestions = 0;
+    currentQuestion = 0;
+    init()
 }
